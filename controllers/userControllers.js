c431629ae48a772be9acc601e7bce7f6336e66dd
yasith9898 <yasithdil.abey@gmail.com>
2024-10-16 10:03:43 +0530

    create .en

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,8 @@
 import User from  '../models/user.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export function postUsers(req,res){
 
--- index.js
+++ index.js
@@ -5,19 +5,23 @@ import userRouter from './routes/usersRoute.js'
 import mongoose from 'mongoose'
 import galleryItemRouter from './routes/galleryItemRoute.js'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 
 
 app.use(bodyParser.json())
 
-const connectionString = "mongodb+srv://tester4:4321@cluster0.zdown.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString = process.env.MONGO_URL;
+
+
 
 app.use((req,res,next)=>{
 
   const token = req.header("Authorization")?.replace("Bearer ", "")
 
   if(token != null){
-    jwt.verify(token,"secret",
+    jwt.verify(token,process.env.JWT_KEY,
       (err,decoded)=>{
       if(decoded != null){
         req.body.user = decoded
